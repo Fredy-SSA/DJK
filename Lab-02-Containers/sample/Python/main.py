6665995042e1e37235dd8fe3159bdde218530e78
Fredy-SSA <alin@zizo.ro>
2020-09-28 17:17:29 +0300

    Lab-2

--- Lab-02-Containers/sample/Python/main.py
+++ Lab-02-Containers/sample/Python/main.py
@@ -1,3 +1,15 @@
-# This program prints Hello, world!
+# Initialize the list
+weekdays = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday","Friday", "Saturday"]
+print("Seven Weekdays are:\n")
+print("--------------------------------")
+print(" ")
+# Iterate the list using for loop
+for day in range(len(weekdays)):
+    print(weekdays[day])
 
-print('Hello, world!')
+print("--------------------------------")
+
+from datetime import date
+
+today = date.today()
+print("Today's date:", today)
